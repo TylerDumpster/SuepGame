c93fcb5991326f46ee29a8cbe41610bae5513ba1
TylerDumpster <104408895+TylerDumpster@users.noreply.github.com>
2022-04-25 22:45:29 -0500

    This is just the game's entry point

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace SuepLeviGame
     internal static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. mmHMm
         /// </summary>
         [STAThread]
         static void Main()
